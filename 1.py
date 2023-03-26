a8435dc2aded565dc57ef104ac5e341249e13162
CarollCheng <carollhwrd@gmail.com>
2023-03-26 17:57:12 +0500

    Horizontal Bar Plot

--- 1.py
+++ 1.py
@@ -5,3 +5,4 @@ dataset_url = ('https://raw.githubusercontent.com/m-mehdi/pandas_tutorials/main/
 df = pd.read_csv(dataset_url, parse_dates=['Date'], index_col='Date')
 df_3Months = df.resample(rule='M').mean()[-3:]
 df_3Months.index=['March', 'April', 'May']
+df_3Months.plot(kind='barh', figsize=(9,6)).get_figure().savefig('name-20')
